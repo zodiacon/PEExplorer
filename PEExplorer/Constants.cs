843c3ee88bde2c4af2626a21ae40115565a87145
Pavel Yosifovich <zodiacon@live.com>
2019-02-13 10:52:42 +0200

    modified version and copyright year

--- PEExplorer/Constants.cs
+++ PEExplorer/Constants.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace PEExplorer {
     static class Constants {
-        public const string AppName = "PE Explorer v0.63";
-        public const string Copyright = "by Pavel Yosifovich (C)2016-2018";
+        public const string AppName = "PE Explorer v0.64";
+        public const string Copyright = "by Pavel Yosifovich (C)2016-2019";
         public static string EmptyTitle = $"{AppName} {Copyright}";
     }
 }
